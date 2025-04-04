78f2086083d796a3b87f036a74564d8a5da82c9d
Spica <Spica>
2025-04-04 15:49:27 +0000

    "create functions"

---
+++ function/67ef8797b8c681002bdc9f21/index.ts
@@ -0,0 +1,11 @@
+export default function (req, res) {
+	return res.status(201).send("Spica is awesome!");
+}
+
+export function funcName (req, res) {
+	return res.status(201).send("Spica is awesome!");
+}
+
+export function funcNameBucket (req, res) {
+	return res.status(201).send("Spica is awesome!");
+}
